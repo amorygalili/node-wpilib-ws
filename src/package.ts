9c6220b872a684c16349ac4a2344cf2e502fb8e9
amorygalili <amorygalili@gmail.com>
2024-06-19 10:14:14 -0400

    Exported interfaces

--- src/package.ts
+++ src/package.ts
@@ -2,6 +2,7 @@ import WPILibWebSocketClient, { WPILibWSClientConfig } from "./wpilib-ws-client"
 export { WPILibWebSocketClient, WPILibWSClientConfig };
 
 import * as WPILibWSMessages from "./protocol/wpilib-ws-proto-messages";
+export * from './protocol/wpilib-ws-proto-messages';
 export { WPILibWSMessages };
 
 import RemoteConnectionInfo from "./remote-connection-info";
