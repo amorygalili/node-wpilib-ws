e056951ae8c2cbdc1c05630b8a6dffbe7a19444b
amorygalili <amorygalili@gmail.com>
2025-07-10 20:27:33 -0400

    Added option in client not to include port

--- src/wpilib-ws-client.ts
+++ src/wpilib-ws-client.ts
@@ -1,94 +1,101 @@
 import ReconnectingWebSocket from "reconnecting-websocket";
 
 import WPILibWSInterface from "./protocol/wpilib-ws-interface";
-import { IWpilibWsMsg, isValidWpilibWsMsg } from "./protocol/wpilib-ws-proto-messages";
+import {
+  IWpilibWsMsg,
+  isValidWpilibWsMsg,
+} from "./protocol/wpilib-ws-proto-messages";
 
 export interface WPILibWSClientConfig {
-    hostname?: string;
-    port?: number;
-    uri?: string;
-    connectOnCreate?: boolean;
-    verbose?: boolean;
+  hostname?: string;
+  port?: number;
+  uri?: string;
+  connectOnCreate?: boolean;
+  verbose?: boolean;
+  noPort?: boolean;
 }
 
 export default class WPILibWebSocketClient extends WPILibWSInterface {
-    private _uri: string = "/wpilibws";
-    private _hostname: string = "localhost";
-    private _port: number = 3300;
-    private _ws: ReconnectingWebSocket;
+  private _uri: string = "/wpilibws";
+  private _hostname: string = "localhost";
+  private _port?: number = 3300;
+  private _noPort?: boolean = false;
+  private _ws: ReconnectingWebSocket;
 
-    constructor(config?: WPILibWSClientConfig) {
-        super();
-        if (config?.hostname) {
-            this._hostname = config.hostname;
-        }
-        if (config?.port) {
-            this._port = config.port;
-        }
-        if (config?.uri) {
-            this._uri = config.uri;
-        }
-
-        if (config?.verbose) {
-            this.verboseMode = config.verbose;
-        }
-
-        if (config?.connectOnCreate) {
-            this.start();
-        }
+  constructor(config?: WPILibWSClientConfig) {
+    super();
+    if (config?.hostname) {
+      this._hostname = config.hostname;
+    }
+    if (config?.port) {
+      this._port = config.port;
+    }
+    if (config?.uri) {
+      this._uri = config.uri;
     }
 
-    public start() {
-        if (this._ready) {
-            return;
-        }
-
-        const url = `ws://${this._hostname}:${this._port}${this._uri}`;
-        this._ws = new ReconnectingWebSocket(url);
-
-        this._ws.addEventListener("open", () => {
-            this._ready = true;
-            console.log("WS Ready");
-            this.emit("ready");
-            this.emit("openConnection");
-        });
-
-        this._ws.addEventListener("close", () => {
-            this._ready = false;
-            console.log("WS Closed");
-            this.emit("closeConnection");
-        });
-
-        this._ws.addEventListener("error", (error) => {
-            console.error(`WS Error ${error.message}`);
-            this._ready = false;
-            this.emit("error", -1, error.message);
-        });
-
-        this._ws.addEventListener("message", msg => {
-            const { data } = msg;
-            try {
-                const msgObj = JSON.parse(data.toString());
-                if (isValidWpilibWsMsg(msgObj)) {
-                    this._handleWpilibWsMsg(msgObj);
-                }
-                else if (this._verbose) {
-                    console.log("Invalid WS message: ", data);
-                }
-            }
-            catch (e) {
-                console.error("Problem parsing JSON", e);
-            }
-        });
+    if (config?.verbose) {
+      this.verboseMode = config.verbose;
     }
 
-    protected _sendWpilibUpdateMessage(msg: IWpilibWsMsg): void {
-        if (this._ready) {
-            if (this._verbose) {
-                console.log("WS MSG (to Robot Code): ", msg);
-            }
-            this._ws.send(JSON.stringify(msg));
-        }
+    if (config?.connectOnCreate) {
+      this.start();
+    }
+    if (config?.noPort) {
+      this._noPort = config.noPort;
+    }
+  }
+
+  public start() {
+    if (this._ready) {
+      return;
     }
 
+    const url = this._noPort
+      ? `ws://${this._hostname}${this._uri}`
+      : `ws://${this._hostname}:${this._port}${this._uri}`;
+    this._ws = new ReconnectingWebSocket(url);
+
+    this._ws.addEventListener("open", () => {
+      this._ready = true;
+      console.log("WS Ready");
+      this.emit("ready");
+      this.emit("openConnection");
+    });
+
+    this._ws.addEventListener("close", () => {
+      this._ready = false;
+      console.log("WS Closed");
+      this.emit("closeConnection");
+    });
+
+    this._ws.addEventListener("error", (error) => {
+      console.error(`WS Error ${error.message}`);
+      this._ready = false;
+      this.emit("error", -1, error.message);
+    });
+
+    this._ws.addEventListener("message", (msg) => {
+      const { data } = msg;
+      try {
+        const msgObj = JSON.parse(data.toString());
+        if (isValidWpilibWsMsg(msgObj)) {
+          this._handleWpilibWsMsg(msgObj);
+        } else if (this._verbose) {
+          console.log("Invalid WS message: ", data);
+        }
+      } catch (e) {
+        console.error("Problem parsing JSON", e);
+      }
+    });
+  }
+
+  protected _sendWpilibUpdateMessage(msg: IWpilibWsMsg): void {
+    if (this._ready) {
+      if (this._verbose) {
+        console.log("WS MSG (to Robot Code): ", msg);
+      }
+      this._ws.send(JSON.stringify(msg));
+    }
+  }
 }
